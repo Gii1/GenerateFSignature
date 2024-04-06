636ad86751c56b3c864c28c6a6f335d0e25b5ff0
BuildTools <unconfigured@null.spigotmc.org>
2024-04-06 23:15:36 +0200

    Changed eq to isequal

--- testReadSignatureFromFunction.m
+++ testReadSignatureFromFunction.m
@@ -5,14 +5,14 @@ root = tree.root;
 % Test1: Empty function
 node1 = root.Next;
 sign1 = FunctionSignature("function1");
-assert(node1 == readSignatureFromFunction(node1));
+assert(isequal(node1, readSignatureFromFunction(node1)));
 
 % Test2: Function with inputs
 node2 = root.Next;
 sign2 = FunctionSignature("function2", input=["var1", "var2"]);
-assert(node2 == readSignatureFromFunction(node2));
+assert(isequal(node2, readSignatureFromFunction(node2)));
 
 % Test3: Function with outputs
 node3 = root.Next;
 sign3 = FunctionSignature("function3", output=["var1", "var2"]);
-assert(node3 == readSignatureFromFunction(node3));
+assert(isequal(node3, readSignatureFromFunction(node3)));
