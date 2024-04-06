602ff7b4077a2bec97e2a717268234c641d5c8fd
BuildTools <unconfigured@null.spigotmc.org>
2024-04-06 23:12:07 +0200

    test functions for the testReadSignatureFronFunction

---
+++ testfunction.m
@@ -0,0 +1,16 @@
+function testfunction; end
+
+% empty function
+function function1()
+    
+end
+
+% function with input
+function function2(var1, var2)
+
+end
+
+% function with output
+function [var1, var2] = function3()
+
+end
