e0c7391a415950bce977903756aa06fe080f5db7
Gii1 <goseija59@gmail.com>
2024-04-07 09:38:28 +0200

    Changed testfunction file to a txt where all functions can be listed

--- testReadSignatureFromFunction.m
+++ testReadSignatureFromFunction.m
@@ -1,18 +1,21 @@
 % Load test functions from file
-tree = mtree("testfunction.m", "-file");
-root = tree.root;
+filetext = string(fileread("testfunction.txt"));
+% split text into seperate function, 
+functiontext = regexp(filetext, "\n(?=function)", "split");
+% first entry are just comments, can be excluded
+functiontext = functiontext(2:end);
 
 % Test1: Empty function
-node1 = root.Next;
+node1 = mtree(functiontext(1), newline);
 sign1 = FunctionSignature("function1");
 assert(isequal(node1, readSignatureFromFunction(node1)));
 
 % Test2: Function with inputs
-node2 = root.Next;
+node2 = mtree(functiontext(2), newline);
 sign2 = FunctionSignature("function2", input=["var1", "var2"]);
 assert(isequal(node2, readSignatureFromFunction(node2)));
 
 % Test3: Function with outputs
-node3 = root.Next;
+node3 = mtree(functiontext(3), newline);
 sign3 = FunctionSignature("function3", output=["var1", "var2"]);
 assert(isequal(node3, readSignatureFromFunction(node3)));
