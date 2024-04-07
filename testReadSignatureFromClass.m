b2d5585c457b4aa016f24dcd3a598170a9dc2fd3
Gii1 <goseija59@gmail.com>
2024-04-07 10:04:02 +0200

    added unit test for the function readSignatureFromClass

---
+++ testReadSignatureFromClass.m
@@ -0,0 +1,16 @@
+% Load test functions from file
+filetext = string(fileread("testclass.txt"));
+% split text into seperate function, 
+classtext = regexp(filetext, "\n(?=classdef)", "split");
+% first entry are just comments, can be excluded
+classtext = classtext(2:end);
+
+% Test 1: Empty class
+node1 = mtree(classtext(1)).root;
+assert(isempty(readSignatureFromClass(node1)));
+
+% Test 2: Class with 2 functions
+node2 = mtree(classtext(2)).root;
+sign2(1) = FunctionSignature("function1");
+sign2(2) = FunctionSignature("function2");
+assert(all(isequal(sign2, readSignatureFromClass(node2))));
