5c6db71948015661ffc74f19e695746d39d1ff9f
BuildTools <unconfigured@null.spigotmc.org>
2024-04-11 20:01:11 +0200

    Changed to function-based unit test

--- unittest/testReadSignatureFromClass.m
+++ unittest/testReadSignatureFromClass.m
@@ -1,18 +1,30 @@
-import gfs.*
+function tests = testReadSignatureFromClass
+    tests = functiontests(localfunctions);
+end
 
-% Load test functions from file
-filetext = string(fileread("testclass.txt"));
-% split text into seperate function, 
-classtext = regexp(filetext, "\n(?=classdef)", "split");
-% first entry are just comments, can be excluded
-classtext = classtext(2:end);
+function setupOnce(testCase)
+    % Add parent to path
+    testCase.applyFixture(matlab.unittest.fixtures.PathFixture("../"))
 
-% Test 1: Empty class
-node1 = mtree(classtext(1)).root;
-assert(isempty(readSignatureFromClass(node1)));
+    % Load test functions from file
+    filetext = string(fileread("testdataReadSignatureFromClass.txt"));
+    % split text into seperate function, 
+    classtext = regexp(filetext, "\n(?=classdef)", "split");
+    % first entry are just comments, can be excluded
+    classtext = classtext(2:end);
+    testCase.TestData.classtext = classtext;
+end
 
-% Test 2: Class with 2 functions
-node2 = mtree(classtext(2)).root;
-sign2(1) = FunctionSignature("class2.function1");
-sign2(2) = FunctionSignature("class2.function2");
-assert(all(isequal(sign2, readSignatureFromClass(node2))));
+function testEmptyClass(testCase)
+    % Test 1: Empty class
+    node1 = mtree(testCase.TestData.classtext(1)).root;
+    testCase.verifyEmpty(readSignatureFromClass(node1));
+end
+
+function testClassWithFunctions(testCase)
+    % Test 2: Class with 2 functions
+    node2 = mtree(testCase.TestData.classtext(2)).root;
+    sign2(1) = FunctionSignature("class2.function1");
+    sign2(2) = FunctionSignature("class2.function2");
+    testCase.verifyEqual(sign2, readSignatureFromClass(node2));
+end
--- unittest/testReadSignatureFromFunction.m
+++ unittest/testReadSignatureFromFunction.m
@@ -1,21 +1,37 @@
-% Load test functions from file
-filetext = string(fileread("testfunction.txt"));
-% split text into seperate function 
-functiontext = regexp(filetext, "\n(?=function)", "split");
-% first entry are just comments, can be excluded
-functiontext = functiontext(2:end);
+function tests = testReadSignatureFromFunction
+    tests = functiontests(localfunctions);
+end
 
-% Test1: Empty function
-node1 = mtree(functiontext(1)).root;
-sign1 = FunctionSignature("function1");
-assert(isequal(node1, readSignatureFromFunction(node1)));
+function setupOnce(testCase)
+    % Add parent to path
+    testCase.applyFixture(matlab.unittest.fixtures.PathFixture("../"))
 
-% Test2: Function with inputs
-node2 = mtree(functiontext(2)).root;
-sign2 = FunctionSignature("function2", input=["var1", "var2"]);
-assert(isequal(node2, readSignatureFromFunction(node2)));
+    % Load test functions from file
+    filetext = string(fileread("testdataReadSignatureFromFunction.txt"));
+    % split text into seperate function 
+    functiontext = regexp(filetext, "\n(?=function)", "split");
+    % first entry are just comments, can be excluded
+    functiontext = functiontext(2:end);    
+    testCase.TestData.functiontext = functiontext;
+end
 
-% Test3: Function with outputs
-node3 = mtree(functiontext(3)).root;
-sign3 = FunctionSignature("function3", output=["var1", "var2"]);
-assert(isequal(node3, readSignatureFromFunction(node3)));
+function testEmptyFunction(testCase)
+    % Test1: Empty function
+    node1 = mtree(testCase.TestData.functiontext(1)).root;
+    sign1 = FunctionSignature("function1");
+    testCase.verifyEqual(readSignatureFromFunction(node1), sign1);
+end
+
+function testFunctionWithInput(testCase)
+    % Test2: Function with inputs
+    node2 = mtree(testCase.TestData.functiontext(2)).root;
+    sign2 = FunctionSignature("function2", input=["var1", "var2"]);
+    testCase.verifyEqual(readSignatureFromFunction(node2), sign2);
+end
+
+function testFunctionWithOutputs(testCase)
+    % Test3: Function with outputs
+    node3 = mtree(testCase.TestData.functiontext(3)).root;
+    sign3 = FunctionSignature("function3", output=["var1", "var2"]);
+    testCase.verifyEqual(readSignatureFromFunction(node3), sign3);
+end
