31f2e70845aa5fe07d57a9a4d382513df3caab24
Gii <goseija59@gmail.com>
2024-04-14 14:00:49 +0200

    implemented readSignatureFromClass

--- +gfs/readSignatureFromClass.m
+++ +gfs/readSignatureFromClass.m
@@ -1,3 +1,20 @@
 function sign = readSignatureFromClass(node)
-    sign = [];
+    % convert a mtree class node into an array of FunctionSignature object
+    sign = gfs.FunctionSignature.empty;
+    name = node.Cexpr.stringval;
+
+    % test for class with no functions
+    if isempty(node.Body); return; end
+
+    % Loop over all function nodes in class
+    functionnode = node.Body.Body;
+    while ~isempty(functionnode)
+        % convert function node to FunctionSignature object
+        funcsign = gfs.readSignatureFromFunction(functionnode);
+        % add class name to the function name
+        funcsign.name = strcat(name, ".", funcsign.name);
+        % add new signature to list
+        sign(end+1) = funcsign;
+        functionnode = functionnode.Next;
+    end
 end
