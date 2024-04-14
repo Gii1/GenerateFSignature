527a59566f96fa8b78be630867193ed6d171c6ff
Gii1 <goseija59@gmail.com>
2024-04-14 22:47:36 +0200

    implemented readSignatureFromFile

--- +gfs/readSignatureFromFile.m
+++ +gfs/readSignatureFromFile.m
@@ -1,4 +1,13 @@
 function sign = readSignatureFromFile(filename)
-    sign = [];
+    node = mtree(filename, "-file").root;
+
+    switch node.kind
+        case "FUNCTION"
+            sign = gfs.readSignatureFromFunction(node);
+        case "CLASSDEF"
+            sign = gfs.readSignatureFromClass(node);
+        otherwise
+            sign = [];
+    end
 end
 
