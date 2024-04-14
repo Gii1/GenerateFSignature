19099256901a038fda3f6b3dc81ea866f0bb3379
Gii <goseija59@gmail.com>
2024-04-14 13:44:52 +0200

    implemented readSignatureFromFunction

--- +gfs/readSignatureFromFunction.m
+++ +gfs/readSignatureFromFunction.m
@@ -1,3 +1,21 @@
 function sign = readSignatureFromFunction(node)
-    sign = [];
+    % convert a mtree function node into a FunctionSignature object
+
+    name = node.Fname.stringval;
+    inputs = string.empty;
+    outputs = string.empty;
+
+    inputnode = node.Ins;
+    while ~isempty(inputnode)
+        inputs(end+1) = string(inputnode.stringval);
+        inputnode = inputnode.Next;
+    end
+
+    outputnode = node.Outs;
+    while ~isempty(outputnode)
+        outputs(end+1) = string(outputnode.stringval);
+        outputnode = outputnode.Next;
+    end
+
+    sign = gfs.FunctionSignature(name, inputs=inputs, outputs=outputs);
 end
