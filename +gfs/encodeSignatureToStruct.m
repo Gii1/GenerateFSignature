5a5c1191d9c405c1ac3bc431e8a15c1abcf3c53b
Gii1 <goseija59@gmail.com>
2024-04-16 17:18:18 +0200

    implemented encodeSignatureToStruct

--- +gfs/encodeSignatureToStruct.m
+++ +gfs/encodeSignatureToStruct.m
@@ -1,3 +1,15 @@
-function jsonstring = encodeSignatureToJson(signature)
-    jsonstring = "";
+function filestruct = encodeSignatureToStruct(signature)
+    for i = 1:length(signature)
+        s = signature(i);
+
+        if ~isempty(s.inputs)
+            functionstruct.inputs = struct("name", {s.inputs.name}, "type", "required", "kind", "");
+        end
+
+        if ~isempty(s.outputs)
+            functionstruct.outputs = struct("name", {s.outputs.name}, "type", "required", "kind", "");
+        end
+
+        filestruct.(s.name) = functionstruct;
+    end
 end
--- +gfs/unittest/testEncodeSignatureToStruct.m
+++ +gfs/unittest/testEncodeSignatureToStruct.m
@@ -5,34 +5,22 @@ end
 function setupOnce(testCase)
     % Add parent to path
     testCase.applyFixture(matlab.unittest.fixtures.PathFixture("../../"))
-
-    % Load the function output from file
-    % The file consists of JSON-snippets, which are splited by %-style
-    % comments
-    filetext = string(fileread("testdataEncodeSignatureToJson.txt"));
-    % split text into seperate function, 
-    jsontext = regexp(filetext, "%.*?\n", "split");
-    % remove first entry and remove trailling whitespaces
-    jsontext = strip(jsontext(2:end));
-    testCase.TestData.jsontext = jsontext;
-end
-
-function testEmptyFunction(testCase)
-    signature = gfs.FunctionSignature("emptyfunction");
-    verifyEqual(testCase, gfs.encodeSignatureToJson(signature), testCase.TestData.jsontext(1));
 end
 
 function testFunctionWithInputs(testCase)
     signature = gfs.FunctionSignature("functionwithinputs", inputs=["var1", "var2"]);
-    verifyEqual(testCase, gfs.encodeSignatureToJson(signature), testCase.TestData.jsontext(2));
+    inputstruct(1) = struct("name", "var1", "type", "required", "kind", "");
+    inputstruct(2) = struct("name", "var2", "type", "required", "kind", "");
+    functionstruct = struct("inputs", inputstruct);
+    filestruct = struct("functionwithinputs", functionstruct);
+    verifyEqual(testCase, gfs.encodeSignatureToStruct(signature), filestruct);
 end
 
 function testFunctionWithOutputs(testCase)
     signature = gfs.FunctionSignature("functionwithoutputs", outputs=["var1", "var2"]);
-    verifyEqual(testCase, gfs.encodeSignatureToJson(signature), testCase.TestData.jsontext(3));
-end
-
-function testFunctionWithInputAndOutput(testCase)
-    signature = gfs.FunctionSignature("functionwithinputandoutput", inputs="var1", outputs="var2");
-    verifyEqual(testCase, gfs.encodeSignatureToJson(signature), testCase.TestData.jsontext(4));
+    outputstruct(1) = struct("name", "var1", "type", "required", "kind", "");
+    outputstruct(2) = struct("name", "var2", "type", "required", "kind", "");
+    functionstruct = struct("outputs", outputstruct);
+    filestruct = struct("functionwithoutputs", functionstruct);
+    verifyEqual(testCase, gfs.encodeSignatureToStruct(signature), filestruct);
 end
