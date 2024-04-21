91c870f4df884deff30d26ca342d43ea94de1bd2
Gii1 <goseija59@gmail.com>
2024-04-21 20:48:22 +0200

    implemented encodeSignatureToJson

---
+++ +gfs/encodeSignatureToJson.m
@@ -0,0 +1,28 @@
+function jsontext = encodeSignatureToJson(signature)
+    % This function encodes an array of FunctionSignature-objects as a
+    % struct which can be parsed using jsonencode to create valid
+    % functionsignature.json files
+
+    % Loop through all signatures
+    for i = 1:length(signature)
+        s = signature(i);
+
+        % Add input 
+        if ~isempty(s.inputs)
+            functionstruct.inputs = table([s.inputs.name]', ...
+                repmat("required", length(s.inputs), 1), ...
+                repmat("", length(s.inputs), 1), ...
+                VariableNames=["name", "kind", "type"]);
+        end
+
+        if ~isempty(s.outputs)
+            functionstruct.outputs = table([s.outputs.name]', ...
+                repmat("", length(s.outputs), 1), ...
+                VariableNames=["name", "type"]);
+        end
+
+        filestruct.(s.name) = functionstruct;
+    end
+
+    jsontext = jsonencode(filestruct);
+end
--- +gfs/unittest/testEncodeSignatureToJson.m
+++ +gfs/unittest/testEncodeSignatureToJson.m
@@ -9,14 +9,14 @@ end
 
 % test signature with 2 input variables
 function testFunctionWithInputs(testCase)
-    signature = gfs.FunctionSignature("functionwithoutputs", inputs=["var1", "var2"]);
+    signature = gfs.FunctionSignature("functionwithinputs", inputs=["var1", "var2"]);
     jsontext = join(readlines("testdataEncodeSignatureToJson\functionwithinputs.json"));
-    verifyEqual(testCase, jsonencode(gfs.encodeSignatureToJson(signature)), jsonencode(jsontext));
+    verifyEqual(testCase, jsondecode(gfs.encodeSignatureToJson(signature)), jsondecode(jsontext));
 end
 
 % test signature with 2 output variables
 function testFunctionWithOutputs(testCase)
     signature = gfs.FunctionSignature("functionwithoutputs", outputs=["var1", "var2"]);
     jsontext = join(readlines("testdataEncodeSignatureToJson\functionwithoutputs.json"));
-    verifyEqual(testCase, jsonencode(gfs.encodeSignatureToJson(signature)), jsonencode(jsontext));
+    verifyEqual(testCase, jsondecode(gfs.encodeSignatureToJson(signature)), jsondecode(jsontext));
 end
