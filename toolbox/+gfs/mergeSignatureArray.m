a9b18c6f5ca4f8e0fbeaa19153c2a33a4a132720
Gii <28624123+Gii1@users.noreply.github.com>
2024-04-30 19:13:57 +0200

    added test for mergesignaturearray

---
+++ toolbox/+gfs/mergeSignatureArray.m
@@ -0,0 +1,6 @@
+function merged = mergeSignatureArray(oldsignature, newsignature)
+    % mergeSignature merges the old signature array read from the signature file
+    % with the new signature array read from the mfiles.
+
+    merged = [];
+end
---
+++ toolbox/+gfs/unittest/testMergeSignatureArray.m
@@ -0,0 +1,22 @@
+function tests = testMergeSignatureArray()
+    tests = functiontests(localfunctions);
+end
+
+% function exist in old signature and not in new
+% output should not include any old signature which is not in the new
+% signature
+function testOnlyInOldSignature(testCase)
+    oldsignature = gfs.FunctionSignature("function");
+    newsignature = gfs.FunctionSignature.empty;
+    outputsignature = gfs.FunctionSignature.empty;
+    testCase.verifyEqual(gfs.mergeSignatureArray(oldsignature, newsignature), outputsignature);
+end
+
+% function exist in new signature and not in old
+% output should include any new signature, even if it not in old
+function testOnlyInNewSignature(testCase)
+    oldsignature = gfs.FunctionSignature.empty;
+    newsignature = gfs.FunctionSignature("function");
+    outputsignature = gfs.FunctionSignature("function");
+    testCase.verifyEqual(gfs.mergeSignatureArray(oldsignature, newsignature), outputsignature);
+end
