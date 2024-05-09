b4520615add861aad19440342725129786a7bb61
Gii1 <28624123+Gii1@users.noreply.github.com>
2024-05-09 23:28:40 +0200

    Implemented readSignatureFromSignatureFile

--- toolbox/+gfs/readSignatureFromSignatureFile.m
+++ toolbox/+gfs/readSignatureFromSignatureFile.m
@@ -1,3 +1,43 @@
-function signature = readSignatureFromSignatureFile(filename)
-    signature = [];
+ function signature = readSignatureFromSignatureFile(filename)
+    % readSignatureFromSignatureFile reads the content of a function
+    % signature file and parses it to an array of FunctionSignature
+    % objects. 
+
+    % read file text
+    filetext = fileread(filename);
+    % parses content to structure object
+    signaturestruct = jsondecode(filetext);
+
+    % get all function names
+    functionnames = string(fieldnames(signaturestruct));
+    % initialize array
+    signature(length(functionnames)) = gfs.FunctionSignature;
+
+    % count all valid function objects
+    k = 0;
+    for i = 1:length(functionnames)
+        % get structure associated with function
+        functionstruct = signaturestruct.(functionnames(i));
+
+        % sort out empty functions
+        if ~isfield(functionstruct, "inputs") && ~isfield(functionstruct, "outputs")
+            break;
+        end
+
+        % create signature object
+        signature(k+1) = gfs.FunctionSignature(functionnames(i));
+
+        % check if input   
+        if isfield(functionstruct, "inputs")
+            signature(i).addInputs(string({functionstruct.inputs.name}), kind=string({functionstruct.inputs.kind}), type=string({functionstruct.inputs.type}));
+        end
+
+        if isfield(functionstruct, "outputs")
+            signature(i).addOutputs(string({functionstruct.outputs.name}), type=string({functionstruct.outputs.type}));
+        end
+
+        k = k + 1;
+    end
+
+    signature = signature(1:k);
 end
