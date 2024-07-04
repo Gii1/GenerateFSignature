0baa34235bd94b69a9ed83755185453370868150
Gii <28624123+Gii1@users.noreply.github.com>
2024-07-04 17:22:53 +0200

    Implemented function

--- toolbox/+gfs/mergeSignatureArray.m
+++ toolbox/+gfs/mergeSignatureArray.m
@@ -2,5 +2,18 @@ function merged = mergeSignatureArray(oldsignature, newsignature)
     % mergeSignature merges the old signature array read from the signature file
     % with the new signature array read from the mfiles.
 
-    merged = [];
+    merged = createArray(size(newsignature), "gfs.FunctionSignature");
+
+    % Loop through all new signatures
+    for i = 1:length(newsignature)
+        sign = newsignature(i);
+        oldsignidx = find(strcmp([oldsignature.name], sign.name), 1);
+
+        % check if the element is also present in the old signature
+        if ~isempty(oldsignidx)
+            merged(i) = gfs.mergeSignature(oldsignature(i), sign);
+        else
+            merged(i) = sign;
+        end
+    end
 end
