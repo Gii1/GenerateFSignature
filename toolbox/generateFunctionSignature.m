4ca9681d0daa5844ac3fdbc44e7c0f942b28e9b8
Gii <28624123+Gii1@users.noreply.github.com>
2024-07-11 16:37:07 +0200

    typo

--- toolbox/generateFunctionSignature.m
+++ toolbox/generateFunctionSignature.m
@@ -12,7 +12,9 @@ function generateFunctionSignature(path)
         return
     end
 
+    oldfileexist = false;
     if isfile(fullfile(path, "resources", "functionSignatures.json"))
+        oldfileexist = true;
         copyfile(fullfile(path, "resources", "functionSignatures.json"), fullfile(path, "resources", "functionSignatures_old.json"))
         oldsign = readSignatureFromSignatureFile(fullfile(path, "resources", "functionSignatures.json"));
         newsign = mergeSignatureArray(oldsign, newsign);
@@ -21,4 +23,12 @@ function generateFunctionSignature(path)
     [~, ~] = mkdir(fullfile(path, "resources"));
 
     gfs.writeSignatureToFile(newsign, fullfile(path, "resources", "functionSignatures.json"));
+
+    disp("Successfully created functionSignatures.json")
+
+    if oldfileexist
+        oldpath = fullfile(path, "resources", "functionSignatures_old.json");
+        newpath = fullfile(path, "resources", "functionSignatures.json");
+        fprintf('Click <a href="matlab:visdiff %s %s">here</a> \n', oldpath, newpath);
+    end
 end
